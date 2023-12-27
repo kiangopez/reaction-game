9322e4016f1d1ddf88c73c3dadc953ca2b4c2e4a
Kian Gopez <kianbgopez@gmail.com>
2023-12-27 15:32:56 +0800

    Full Project

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>Reaction Time: {{ score }} ms</p>
+  <div>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score <= 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score <= 300) {
+      this.rank = "Fast but not quick";
+    } else {
+      this.rank = "You got frozen fingers?";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
